59571042b2c0ff2555d2e2f0ec73913bfdfca6ea
balaji0045 <balajidk100@gmail.com>
2024-06-26 22:09:41 +0530

    Scale loader Feature Updated!..

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Main from './components/main/Main';
 import Footer from './components/Footer/Footer';
 import Blogs from './pages/Blogs';
+import { ScaleLoader } from 'react-spinners';
 function App() {
-  
+    const [loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+   setTimeout(() => {
+    setLoading(false)
+   }, 3000);
+  },[])
   
   return (
     <div className="App">
+    {loading ?(
+      <div className="loader" style={{
+        minHeight:'100vh',
+        display:'flex',
+        alignItems:'center',
+        backgroundColor:'black',
+        justifyContent:'center'
+      }}>
+
+      <ScaleLoader color='white'
+        height={'100px'}
+        width={'8px'}
+      />
+      </div>
+      
+      ):(
       <Router>
         {/* <Main className='main'/> */}
       <Routes >
@@ -17,6 +40,7 @@ function App() {
         <Route path='/blogs' element={<Blogs/>} />
       </Routes>
       </Router>
+  )}
     </div>
   );
 }
